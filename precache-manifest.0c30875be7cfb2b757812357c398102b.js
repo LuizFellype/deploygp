393d4df35f703f9c7de1daa0f44495f184e6904b
Luiz Felipe <dylon-cassago@hotmail.com>
2018-10-30 02:26:58 -0300

    Updates

---
+++ precache-manifest.0c30875be7cfb2b757812357c398102b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploygp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cce5f68fc7fa1d69168f",
+    "url": "/deploygp/static/js/runtime~main.cce5f68f.js"
+  },
+  {
+    "revision": "c9bc6044debc8f113cbf",
+    "url": "/deploygp/static/js/main.c9bc6044.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/deploygp/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "c9bc6044debc8f113cbf",
+    "url": "/deploygp/static/css/main.b7bab088.chunk.css"
+  },
+  {
+    "revision": "0367ae100dd93df88123457c3c850376",
+    "url": "/deploygp/index.html"
+  }
+];
